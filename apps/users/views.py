6a97a1efdd7b3e2860ce647d1c17ea5c24d52168
dyak <artemdyakonov17@mail.ru>
2024-08-01 21:34:33 +0400

    v4

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,20 +1,34 @@
 from django.contrib.auth.views import LoginView
 from django.views.generic import FormView
 from .forms import UserRegisterForm
+from django.urls import reverse_lazy
 
 
 class UserLoginView(LoginView):
     template_name = "users/login.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(
+            UserLoginView, self
+        ).get_context_data(**kwargs)
+
+        context['bgimage'] = 'img/фон.jpg'
+        return context
+
 
 class UserRegisterView(FormView):
     template_name = "users/register.html"
     form_class = UserRegisterForm
+    success_url = reverse_lazy("users_login")
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super(
             UserRegisterView, self
         ).get_context_data(**kwargs)
 
-        context['bgimage'] = 'img/пляж.jpg'
+        context['bgimage'] = 'img/экран.jpg'
         return context
